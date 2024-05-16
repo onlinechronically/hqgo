fa128434ce5902e02d602291eefffebe22ce9f21
lillie <onlinechronically@protonmail.com>
2024-05-16 13:54:36 -0400

    add: RequestTypes file for structs

---
+++ RequestTypes.go
@@ -0,0 +1,37 @@
+package hqtrivia
+
+// POST /verify-existing-phone
+type VerificationStart struct {
+	Method string `json:"method"`
+	Phone  string `json:"phone"`
+}
+
+// POST /apiUrl/verifications/<verificationID>
+type VerificationFinish struct {
+	Code string `json:"code"`
+}
+
+// POST /usernames/available
+type CheckUsername struct {
+	Username string `json:"username"`
+}
+
+// POST /referral-code/valid
+type CheckReferral struct {
+	ReferralCode string `json:"referralCode"`
+}
+
+// POST /tokens
+type RefreshTokens struct {
+	Token string `json:"token"`
+}
+
+// POST /users
+type RegisterAccount struct {
+	Country           string `json:"country"`
+	TimeZone          string `json:"timeZone"`
+	Locale            string `json:"locale"`
+	ReferringUsername string `json:"referringUsername,omitempty"`
+	Username          string `json:"username"`
+	VerificationID    string `json:"verificationId"`
+}
